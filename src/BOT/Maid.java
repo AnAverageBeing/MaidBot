016125f5df7592e9a9d5dfa83f289181d910eb46
AnAverageBeing <yatharathi.rathi@gmail.com>
2022-12-24 11:55:34 +0530

    lol

--- src/BOT/Maid.java
+++ src/BOT/Maid.java
@@ -1,8 +1,8 @@
 package BOT;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
-import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import BOT.utils.GetToken;
 
@@ -10,12 +10,11 @@ import javax.security.auth.login.LoginException;
 
 public class Maid {
     public static String prefix = ".";
-    private static ShardManager manager;
     public static void main(String[] args) throws LoginException {
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(GetToken.get());
+        JDABuilder builder = JDABuilder.createDefault(GetToken.get());
         builder.setMemberCachePolicy(MemberCachePolicy.ALL);
         builder.enableIntents(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS));
-        manager = builder.build();
-        manager.addEventListener(new MaidListener());
+        JDA BOT = builder.build();
+        BOT.addEventListener(new MaidListener());
     }
 }
